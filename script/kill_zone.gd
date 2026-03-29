d32a1680e9cd4f6d569fb28c47522da3e16c1282
darkicestormteam <darkicestormteam@gmail.com>
2026-03-29 14:13:26 +0300

    tt

--- script/kill_zone.gd
+++ script/kill_zone.gd
@@ -8,7 +8,7 @@ func _on_body_entered(body: Node2D) -> void:
 		# Находим узел GameOver в сцене через глобальный поиск и делаем его видимым
 		var game_over = get_tree().get_first_node_in_group("game_over")
 		if not game_over:
-			game_over = get_node("/root/game/GameOver")
+			game_over = get_node("/root/GameOver")
 		if not game_over:
 			game_over = get_tree().current_scene.get_node_or_null("GameOver")
 		
