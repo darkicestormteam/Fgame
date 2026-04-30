b79b9fc9b68d3ed5d94c027ee7cc359ce863db87
darkicestormteam <darkicestormteam@gmail.com>
2026-04-30 16:50:32 +0300

    ee

--- script/spellmenu.gd
+++ script/spellmenu.gd
@@ -3,20 +3,10 @@ extends CanvasLayer
 
 var is_visible: bool = false
 
-@onready var tap_sound: AudioStreamPlayer = $Tap
-@onready var spell_sheep_btn: TextureButton = $MarginContainer/HBoxContainer/SpellSheep
-@onready var sword_up_btn: TextureButton = $MarginContainer/HBoxContainer/SwordUP
-@onready var test_btn: TextureButton = $MarginContainer/HBoxContainer/Test
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	# Скрываем меню при старте
 	visible = false
-	
-	# Подключаем сигналы нажатия кнопок
-	spell_sheep_btn.pressed.connect(_on_button_pressed)
-	sword_up_btn.pressed.connect(_on_button_pressed)
-	test_btn.pressed.connect(_on_button_pressed)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -27,13 +17,3 @@ func show_spellmenu() -> void:
 	visible = true
 	is_visible = true
 	get_tree().paused = true
-
-func _on_button_pressed() -> void:
-	# Воспроизводим звук нажатия
-	tap_sound.play()
-	
-	# Продолжаем игру через 0.5 секунд
-	await get_tree().create_timer(0.5).timeout
-	get_tree().paused = false
-	visible = false
-	is_visible = false
