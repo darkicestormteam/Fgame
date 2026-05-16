7490198b1397f6ed77a14307efd831f3757d6dd7
darkicestormteam <darkicestormteam@gmail.com>
2026-05-16 23:42:08 +0300

    313

--- script/tree.gd
+++ script/tree.gd
@@ -8,21 +8,26 @@ var is_disabled: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	add_to_group("Tree")
+		add_to_group("Tree")
 
 # Вызывается при взаимодействии с врагом - отключает анимацию и коллизию, включает статичный спрайт
 func disable_tree() -> void:
-	if is_disabled:
-		return
-	is_disabled = true
-	if animated_sprite:
-		animated_sprite.visible = false
-	if collision_shape:
-		collision_shape.disabled = true
-	if sprite:
-		sprite.visible = true
+		if is_disabled:
+				return
+		is_disabled = true
+		if animated_sprite:
+				animated_sprite.visible = false
+		if collision_shape:
+				collision_shape.set_deferred("disabled", true)
+				# Дополнительно убираем форму для гарантии
+				collision_shape.shape = null
+		if sprite:
+				sprite.visible = true
+		# Также отключаем коллизии на самом StaticBody2D
+		collision_layer = 0
+		collision_mask = 0
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+		pass
